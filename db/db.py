e17d530fe5232a37cba85c3fe4353640354add39
Max Luzuriaga <max@luzuriaga.com>
2015-07-01 13:41:35 -0400

    Fixed db initialization code.

--- db/db.py
+++ db/db.py
@@ -8,12 +8,10 @@ def connect_db():
     return sqlite3.connect('./db/valuebot.sqlite3')
 
 def init_db():
-    # TODO: FIX THIS
-    with app.app_context():
-        with closing(connect_db()) as db:
-            with app.open_resource('./db/schema.sql', mode='r') as f:
-                db.cursor().executescript(f.read())
-            db.commit()
+    with closing(connect_db()) as db:
+        with file('./db/schema.sql') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 class Post():
     def __init__(self, user, poster, value, text):
