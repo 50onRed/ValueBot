5608ccdfd4948db6143de1462023e01edebb8101
Max Luzuriaga <max@luzuriaga.com>
2015-07-17 15:35:48 -0400

    Changed sqlalchemy usage to not use flask-sqlalchemy.

--- db/db.py
+++
@@ -1,6 +0,0 @@
-import config
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
-
-db = create_engine(config.SQLALCHEMY_DATABASE_URI)
-Base = declarative_base()
